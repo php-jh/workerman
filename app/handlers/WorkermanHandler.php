440d154ed05b388711590a3637d4b44f7a88cbab
jiang <1169363434@qq.com>
2019-01-23 17:54:01 +0800

    解决通信demo

--- app/handlers/WorkermanHandler.php
+++ app/handlers/WorkermanHandler.php
@@ -8,6 +8,7 @@
 
 namespace App\handlers;
 
+use Illuminate\Support\Facades\Redis;
 use Workerman\Lib\Timer;
 
 // 心跳间隔10秒
@@ -16,45 +17,50 @@ define('HEARTBEAT_TIME', 10);
 class WorkermanHandler
 {
     // 处理客户端连接
-    public function onConnect($connection)
+    public function onConnect()
     {
-        echo "new connection from ip " . $connection->getRemoteIp() . "\n";
+
     }
 
     // 处理客户端消息
     public function onMessage($connection, $data)
     {
-        var_dump($connection->id);
-//        // 向客户端发送hello $data
+       // 向客户端发送hello $data
         $val = json_decode($data,true);
         $user_id = !empty($val['user_id'])?$val['user_id']:0;
         if(empty($user_id)){
             $result = json_encode(array('code'=>401,'msg'=>'缺少user_id'));
             $connection->send('Hello, your send message is: ' . $result);
         }
-        $cline_id = !empty($val['cline_id'])?$val['cline_id']:$connection->id;
-//        $result_cline_id = \DB::table('cline')->select('user_id')->where('cline_id',$cline_id)->first();
+        $type = !empty($val['type'])?$val['type']:'login';
+        switch ($type){
+            case 'login':
+                Redis::hmset('webscoket_client_id:',$user_id,$connection->id);
 
-        \DB::table('cline')->insert([
-            'user_id'=>$user_id,
-            'cline_id'=>$cline_id,
-            'data'=>json_encode($connection)
-        ]);
+                Redis::hmset('webscoket_user_id:',$connection->id,$user_id);
 
-//        $result = \DB::table('api_token')->select('id')->where('answer_token',$token)->first();
-//        if(empty($result)){
-//            $result = json_encode(array('code'=>400,'msg'=>'异地登录'));
-//        }else{
-//            $result = json_encode(array('code'=>200,'msg'=>'异地登录'));
-//        }
-        $connection->send('Hello, your send message is: ' . (string)$cline_id);
+                foreach($connection->worker->connections as $connection)
+                {
+                    $connection->send($connection->id.'登录3user_id'.$user_id);
+                }
+                break;
+            case 'message':
+                $message = !empty($val['name'])?$val['name']:'等待消息';
+                foreach($connection->worker->connections as $connection)
+                {
+                    $connection->send($user_id.'发送消息'.$message);
+                }
+                break;
+            default:
+                echo '无效';
+        }
 
     }
 
     // 处理客户端断开
     public function onClose($connection)
     {
-        $connection->send('断开连接了',$connection);
+        echo '断开连接了'.$connection->id;
     }
 
     public function onWorkerStart($worker)
@@ -69,7 +75,7 @@ class WorkermanHandler
                 }
                 // 上次通讯时间间隔大于心跳间隔，则认为客户端已经下线，关闭连接
                 if ($time_now - $connection->lastMessageTime > HEARTBEAT_TIME) {
-                    echo "Client ip {$connection->getRemoteIp()} timeout!!!\n";
+//                    $connection->send($connection->id.'发送消息时间:'.time());
                     $connection->close();
                 }
             }
